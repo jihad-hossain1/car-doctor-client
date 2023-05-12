b7ecc513e65270705e800deb4788a983e30b631d
jihad-hossain1 <jihadkhan934@gmail.com>
2023-05-12 11:52:22 +0600

    checkout-> booking order -> send to database

--- src/pages/checkOut/CheckOut.jsx
+++ src/pages/checkOut/CheckOut.jsx
@@ -1,9 +1,112 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../provider/AuthProvider";
 
 const CheckOut = () => {
+  const service = useLoaderData();
+  const { title, _id, price, img } = service;
+  const { user } = useContext(AuthContext);
+
+  const handleBookUser = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const price = form.price.value;
+    const date = form.date.value;
+    const email = form.email.value;
+    const order = {
+      customerName: name,
+      email: email,
+      date: date,
+      price: price,
+      service_id: _id,
+      service: title,
+      image: img,
+    };
+    console.log(order);
+    fetch("http://localhost:5000/bookings", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(order),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
   return (
-    <div>
-      <h2 className="text-center font-semibold text 3xl">Book Services </h2>
+    <div className="p-10">
+      <h2 className="text-center font-semibold text-3xl py-8 text-gray-600">
+        <span className="underline">Book Service:</span> {title}{" "}
+      </h2>
+
+      <div>
+        <form onSubmit={handleBookUser}>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="name"
+                defaultValue={user?.displayName}
+                name="name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Date</span>
+              </label>
+              <input
+                type="date"
+                name="date"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                defaultValue={user?.email}
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Due amount</span>
+              </label>
+              <input
+                type="text"
+                defaultValue={"$" + price}
+                className="input input-bordered"
+                name="price"
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+          </div>
+          <div className="form-control mt-6">
+            <input
+              type="submit"
+              value="Order Confirm"
+              className="btn btn-primary btn-block"
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
