8d92963ca12e06644291b9fdbb31ef6da20dcb00
jihad-hossain1 <jihadkhan934@gmail.com>
2023-05-13 01:06:26 +0600

    delete from ui and database in order section

--- src/pages/booking/Bookings.jsx
+++ src/pages/booking/Bookings.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../provider/AuthProvider";
+import TableRow from "./TableRow";
 
 const Bookings = () => {
   const { user } = useContext(AuthContext);
@@ -15,9 +16,56 @@ const Bookings = () => {
       });
   }, []);
 
+  const handleDelete = (id) => {
+    const proceed = confirm("Are you sure you want to delete");
+    if (proceed) {
+      fetch(`http://localhost:5000/bookings/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            alert("deleted successfull");
+            const remaning = bookings.filter((booking) => booking._id !== id);
+            setBookings(remaning);
+          }
+        });
+    }
+  };
+
   return (
     <div>
       <h3>Total Bookings: {bookings.length}</h3>
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr className="">
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Service</th>
+              <th>Date</th>
+              <th>Price</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody className="">
+            {bookings.map((booking) => (
+              <TableRow
+                key={booking._id}
+                booking={booking}
+                handleDelete={handleDelete}
+              ></TableRow>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
---
+++ src/pages/booking/TableRow.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const TableRow = ({ booking, handleDelete }) => {
+  const { _id, customerName, email, date, price, service_id, service, image } =
+    booking;
+
+  return (
+    <div>
+      <tr>
+        <th>
+          <button
+            className="btn btn-sm btn-circle"
+            onClick={() => handleDelete(_id)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </th>
+        <td>
+          <div className="avatar">
+            <div className="rounded w-24 h-24">
+              <img src={image} alt="Avatar Tailwind CSS Component" />
+            </div>
+          </div>
+        </td>
+        <td className="font-semibold">{customerName}</td>
+        <td className="text-gray-600 underline">{service}</td>
+        <td>{price}</td>
+        <td>{date}</td>
+        <th>
+          <button className="btn btn-ghost btn-xs">details</button>
+        </th>
+      </tr>
+    </div>
+  );
+};
+
+export default TableRow;
